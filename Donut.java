04185abffa6b49d0c54cc050da0fe0b4cd7fd458
Lynchz <100924535+LynchzDEV@users.noreply.github.com>
2023-08-08 14:28:04 +0700

    Update Donut.java

--- Donut.java
+++ Donut.java
@@ -6,11 +6,19 @@ public class Donut {
     public static void main(String[] args) throws InterruptedException {
         int k;
         double A = 0, B = 0, i, j;
-        double[] z = new double[1920];
-        char[] b = new char[1920];
-        for (; ; ) {
-            Arrays.fill(b, 0, 1920, ' ');
-            Arrays.fill(z, 0, 1920, 0);
+
+        // Adjustable variables
+        int screenWidth = 80; // Number of columns in the output screen
+        int screenHeight = 25; // Number of rows in the output screen
+        double rotationSpeedA = 0.03; // Speed of rotation for variable A
+        double rotationSpeedB = 0.01; // Speed of rotation for variable B
+        int frameDelay = 10; // Delay between frames in milliseconds
+
+        double[] z = new double[screenWidth * screenHeight];
+        char[] b = new char[screenWidth * screenHeight];
+        for (;;) {
+            Arrays.fill(b, 0, screenWidth * screenHeight, ' ');
+            Arrays.fill(z, 0, screenWidth * screenHeight, 0);
             for (j = 0; 6.28 > j; j += 0.07)
                 for (i = 0; 6.28 > i; i += 0.02) {
                     double c = Math.sin(i),
@@ -26,21 +34,21 @@ public class Donut {
                             t = c * h * g - f * e;
                     int x = (int) (40 + 30 * D * (l * h * m - t * n)),
                             y = (int) (12 + 15 * D * (l * h * n + t * m)),
-                            o = x + 80 * y,
+                            o = x + screenWidth * y,
                             N = (int) (8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n));
-                    if (y >= 0 && y < 24 && x >= 0 && x < 80 && D > z[o]) {
+                    if (y >= 0 && y < screenHeight && x >= 0 && x < screenWidth && D > z[o]) {
                         z[o] = D;
-                        b[o] = new char[]{'.', ',', '-', '~', ':', ';', '=', '!', '*', '#', '$', '@'}[Math.max(N, 0)];
+                        b[o] = new char[] { '.', ',', '-', '~', ':', ';', '=', '!', '*', '#', '$', '@' }[Math.max(N,
+                                0)];
                     }
                 }
             System.out.print("\u001b[H");
-            for (k = 0; 1920 > k; k++)
-                System.out.print(k % 80 > 0 ? b[k] : 10);
-            A += 0.04;
-            B += 0.02;
+            for (k = 0; screenWidth * screenHeight > k; k++)
+                System.out.print(k % screenWidth > 0 ? b[k] : 10);
+            A += rotationSpeedA;
+            B += rotationSpeedB;
 
-            // Add a short delay between frames (adjust the value in milliseconds as needed)
-            Thread.sleep(50);
+            Thread.sleep(frameDelay);
         }
     }
 }
